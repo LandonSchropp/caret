0d721f9347486acf7a86b1df0634a018bb345226
Landon Schropp <schroppl@gmail.com>
2012-09-30 12:40:31 -0700

    Created an outline for the caret plugin.

--- jquery.caret.js
+++ jquery.caret.js
@@ -1,4 +1,4 @@
-(function( $ ){
+(function($){
 
   /*
   If the position parameter is not provided, this method returns the position of the caret for the 
@@ -7,11 +7,11 @@
   position - The new position of the caret. If this value is less than 0 or greater than the length
   of the content of this HTML element, it will be clamped.
   */
-  $.fn.maxHeight = function(position) {
+  $.fn.caret = function(position) {
   
     
 
     // return this so the calls may be chained
     return this;
   };
-})( jQuery );
+})(jQuery);
